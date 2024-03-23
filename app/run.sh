74565eb6401bfad5516065215b34e86677b531e6
JoseC14 <josecarlosv8474@gmail.com>
2024-03-23 16:58:28 -0300

    Modificado run.sh

--- app/run.sh
+++ app/run.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
+
+cd ../
+pwd
 pip install django
 pip install djangorestframework
 pip install django-cors-headers
 
-npm install @vue/cli
+npm install -g @vue/cli
+npm install axios
 
+cd app/frontend/core
+npm install
+
+cd ../../
 cd backend
 python manage.py runserver 3000 &
 
 cd ../
 cd frontend/core
 npm run serve
+
